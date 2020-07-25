d7e6bbfe1b2539a84dfe1d8841fde0da24b0cc5e
Ron Pearl <ron@ronpearl.com>
2020-07-25 11:02:36 -0400

    Cleanup

--- FileDedup.Core/MainProgram.cs
+++ FileDedup.Core/MainProgram.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-// using Menu;
 
 namespace FileDedup.Core
 {
--- FileDedup.Core/Menu.cs
+++ FileDedup.Core/Menu.cs
@@ -7,7 +7,6 @@
 /////////////////////////////////////////
 
 using System;
-//using System.Linq.Expressions;
 
 namespace FileDedup.Core
 
