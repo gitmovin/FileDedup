4467cccc65db423e11eedefbe504cb169b0103fe
Ron Pearl <ron@ronpearl.com>
2020-08-04 15:18:22 -0400

    Refined rejection criteria

--- FileDedup.Core/ProcessFiles.cs
+++ FileDedup.Core/ProcessFiles.cs
@@ -23,12 +23,15 @@ namespace FileDedup.Core
             int count1MM = 0;
             int count2MBook = 0;
             int count3MBPro = 0;
-
+            bool createNewFolderAndFile;
+            string sourceString, destinationString;
             // Stacks for containing processed strings representing directories
             Stack<string> myStack0 = new Stack<string>();
             Stack<string> myStack1 = new Stack<string>();
             Stack<string> myStack2 = new Stack<string>();
             Stack<string> myStack3 = new Stack<string>();
+            Stack<string> myStack4 = new Stack<string>();
+            Stack<string> myStack5 = new Stack<string>();
 
 
             // Set the variable "dirs" to contain a list of all directories under the "root" directory.
@@ -38,7 +41,7 @@ namespace FileDedup.Core
             // Print the list of directories to a text file.
             for (int i=0; i < dirs.Count; i++)
             {
- 
+                createNewFolderAndFile = true;
                 // Drop directories that need not be saved
                 if (dirs[i].Contains("RECYCLE.BIN")
                     || dirs[i].Contains(@"\Desktop")
@@ -50,34 +53,52 @@ namespace FileDedup.Core
                     || dirs[i].Contains(@"\from 2MBook\Users\apple\src\ltcadm")
                     || dirs[i].Contains(@"\from 2MBook\Users\apple\src\ltcuat")
                     || dirs[i].Contains(@"\from 2MBook\Users\apple\tmp")
+                    || dirs[i].Contains(@"\Address Book -")
+                    || dirs[i].Contains(@"\ScanSnap\")
+                    || dirs[i].Contains(@"z_backups\")
+                    || dirs[i].Contains(@".DS_Store")
+                    || dirs[i].Contains(@"\z_Archive")
+                    || dirs[i].Contains(@"trash")
                     )
                 {
-                    Console.Write("DELETE              : ");
+//                    Console.Write("DELETE              : ");
                     myStack0.Push(dirs[i]);
+                    createNewFolderAndFile = false;
                     countDeleted++;
                 } else
                 // put remaining directories in one of three lists
                 if (dirs[i].Contains("from 1MM"))
                 {
-                    Console.Write("write to 1MM list   : ");
+//                    Console.Write("write to 1MM list   : ");
                     myStack1.Push(dirs[i]);
                     count1MM++;
                 } else
                if (dirs[i].Contains("from 2MBook"))
                 {
-                    Console.Write("write to 2MBook list: ");
+ //                  Console.Write("write to 2MBook list: ");
                     myStack2.Push(dirs[i]);
                     count2MBook++;
                 } else
                if (dirs[i].Contains("from 3MBPro"))
                 {
-                    Console.Write("write to 3MBPro list: ");
+ //                   Console.Write("write to 3MBPro list: ");
                     myStack3.Push(dirs[i]);
                     count3MBPro++;
                 }
-                Console.WriteLine(dirs[i]);
+                // Write to files that create folders and files
+ //               Console.Write(\" Line(dirs[i]);
+                if (createNewFolderAndFile)
+                {
+                    sourceString= dirs[i];
+                    destinationString = sourceString.Replace("TEMP", "TEMP_OUT");
+                     string line1 = "mkdir " + "\"" + destinationString + "\"";
+                    string line2 = "copy " + "\"" + sourceString + "\\" + "*.*" + "\"" + " " + "\"" + destinationString + "\"";
+                    myStack4.Push(line1);
+                    myStack5.Push(line2);
+                }
             }
 
+            // Print summary on console
             Console.WriteLine("countDeleted = {0}", countDeleted);
             Console.WriteLine("count1MM = {0}", count1MM);
             Console.WriteLine("count2MBook = {0}", count2MBook);
@@ -94,15 +115,34 @@ namespace FileDedup.Core
             Array.Sort(myStack2Sort);
             string[] myStack3Sort = myStack3.ToArray();
             Array.Sort(myStack3Sort);
+            string[] myStack4Sort = myStack4.ToArray();
+            Array.Sort(myStack4Sort);
+            string[] myStack5Sort = myStack5.ToArray();
+            Array.Sort(myStack5Sort);
 
             // Create sorted text files
             System.IO.File.WriteAllLines(@"c:\zz_TEMP\working folder\filesDeleted.txt", myStack0Sort);
             System.IO.File.WriteAllLines(@"c:\zz_TEMP\working folder\files1MM.txt", myStack1Sort);
             System.IO.File.WriteAllLines(@"c:\zz_TEMP\working folder\files2MBook.txt", myStack2Sort);
             System.IO.File.WriteAllLines(@"c:\zz_TEMP\working folder\files3MBPro.txt", myStack3Sort);
+            System.IO.File.WriteAllLines(@"c:\zz_TEMP\working folder\makeDir.bat", myStack4Sort);
+            System.IO.File.WriteAllLines(@"c:\zz_TEMP\working folder\makeFile.bat", myStack5Sort);
 
             Console.WriteLine("Writing to text files complete");
 
         }
+ /*       // Replace substring
+        ReplaceSubstring(string masterString, string fromString, string toString)
+        {
+            // Convert string to character array
+            string source = "The mountains are behind the clouds today.";
+
+            // Replace one substring with another with String.Replace.
+            // Only exact matches are supported.
+            var replacement = source.Replace("mountains", "peaks");
+            Console.WriteLine($"The source string is <{source}>");
+            Console.WriteLine($"The updated string is <{replacement}>");
+        }
+*/
     }
 }
